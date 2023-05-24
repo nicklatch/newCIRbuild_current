78dfd1f6d0acabeaddd07fad637ffa4b6fe05b45
Nick <nick.latcham@proton.me>
2023-05-24 12:37:49 -0500

    .

--- src/services/pointsService.js
+++ src/services/pointsService.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
-
+import fwdData from '../../fwdDrivers.json';
+import rwdData from '../../rwdDrivers.json';
 //TODO: create services for fetching points
 
 const getAllFwd = async () => {
-  const response = await axios.get('../../public/fwdDrivers.json');
-  return response.data;
+  // const response = await axios.get('../../fwdDrivers.json');
+  const response = fwdData;
+  return response;
 };
 
 const getAllRwd = async () => {
-  const response = await axios.get('../../public/rwdDrivers.json');
+  // const response = await axios.get('../../rwdDrivers.json');
+  const response = rwdData;
   return response.data;
 };
 
