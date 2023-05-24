8487a41338a58e4c0e77b092ceaf6a62e0786701
Nick <nick.latcham@proton.me>
2023-05-24 12:51:20 -0500

    fixed the undefined return for getAllRwd() call

--- src/services/pointsService.js
+++ src/services/pointsService.js
@@ -1,18 +1,18 @@
-import axios from 'axios';
+// import axios from 'axios';
 import fwdData from '../../fwdDrivers.json';
 import rwdData from '../../rwdDrivers.json';
 //TODO: create services for fetching points
 
 const getAllFwd = async () => {
   // const response = await axios.get('../../fwdDrivers.json');
-  const response = fwdData;
+  const response = await fwdData;
   return response;
 };
 
 const getAllRwd = async () => {
   // const response = await axios.get('../../rwdDrivers.json');
-  const response = rwdData;
-  return response.data;
+  const response = await rwdData;
+  return response;
 };
 
 export default { getAllFwd, getAllRwd };
